7d10b19660ee92d98e373108a862da80da940032
Matthieu Nzinga <matthieunzinga@gmail.com>
2024-08-11 11:06:01 +0100

    project configuration

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex h-screen'>
+        <div className='w-[55%] bg-custom-gradient p-10'>hello</div>
+        <div className='w-[45%] bg-blue-500'>hello</div>
+    </div>
+  )
+}
+
+export default Login
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(90deg, #E0EAFF 0%, #F3E0FF 100%)',
+      },
+    },
+  },
+  plugins: [],
+};
