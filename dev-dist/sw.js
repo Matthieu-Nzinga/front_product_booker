d83c82719ff985ad346f00e6eef8673225e99644
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 <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>
2024-08-27 22:32:21 +0100

    fix create user

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gh6v9t50pbg"
+    "revision": "0.d66ri0jsb6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
